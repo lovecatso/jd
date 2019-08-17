dee5c6b25a2b19e131f2fce96732b6259a4a08da
xuhongjie <xuhongjie@csii.com.cn>
2019-08-17 11:05:08 +0800

    背单词项目

---
+++ js/dictionary.js
@@ -0,0 +1,23 @@
+function MyDictionary(){
+    return [
+        {id:1,mega:'七龙珠改',episodes:[
+            {no:'1',words:[
+                {word:'dawdle',edefinition:'to  move or act too slowly',notes:'闲荡',examples:''},
+                {word:'eyewitness',edefinition:'',notes:'目击者',examples:''},
+                {word:'commotion',edefinition:'a noisy excitement and confusion',notes:'(突然发生的) 喧闹',examples:''},
+            ]},
+            {no:'2',words:[]},
+            {no:'3',words:[]}
+        ]},
+        {id:2,mega:'科学电磁炮',episodes:[
+            {no:'1',words:[]},
+            {no:'2',words:[]},
+            {no:'3',words:[]}
+        ]},
+        {id:3,mega:'蜡笔小新',episodes:[
+            {no:'1',words:[]},
+            {no:'2',words:[]},
+            {no:'3',words:[]}
+        ]}
+    ]
+}
