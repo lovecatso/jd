7e5656e65196da603661641d4f3c4fce0a1f9ef5
xuhongjie <xuhongjie@csii.com.cn>
2019-08-20 15:03:28 +0800

    背单词功能优化

--- js/dictionary.js
+++ js/dictionary.js
@@ -5,9 +5,56 @@ function MyDictionary(){
                 {word:'dawdle',edefinition:'to  move or act too slowly',notes:'闲荡',examples:''},
                 {word:'eyewitness',edefinition:'',notes:'目击者',examples:''},
                 {word:'commotion',edefinition:'a noisy excitement and confusion',notes:'(突然发生的) 喧闹',examples:''},
+                
             ]},
-            {no:'2',words:[]},
-            {no:'3',words:[]}
+            {no:'2',words:[
+                {word:'alias',edefinition:'',notes:'化名',examples:'a fugitive using several alias'},
+                {word:'pterodactyl',edefinition:'',notes:'翼龙',examples:''},
+                {word:'ransom',edefinition:'',notes:'赎金',examples:''},
+                {word:'lame',edefinition:'',notes:'美语非正式 not smart,not impressive',examples:'her joke is lame'},
+                {word:'klutz',edefinition:'a clumsy person',notes:'笨手笨脚的人',examples:'i am a complete klutz on stage'}
+                
+            ],
+            phrases:[
+               {phrase:'lose one\'s nerve',edefinition:'become afraid',notes:'开始慌张害怕'},
+               {phrase:'can\'t hold a candle to',edefinition:'',notes:'不能与..相提并论，不在一个级别）手持蜡烛为他人照明的工作被看作是不用脑筋而低人一等的差事'},
+               {phrase:'fit in with',edefinition:'to belong in a particular situation place or gtoup',note:'',examples:'i am looking for a group  i can fit in with'} 
+            ],
+            questions:[
+                'pitch in to 和chip in有什么区别,都是不正式么'
+            ]
+
+        },
+            {no:'3',words:[
+            {word:'reflex',edefinition:'an action or movement of the body that happens automatically as a reaction to something',notes:'本能反射',examples:''},
+             {word:'delinquent',edefinition:'a young person who regularly does illegal or immoral things',notes:'少年罪犯',examples:''},
+            {word:'notice',edefinition:'',notes:'',examples:''},
+            {word:'lewd',edefinition:'sexual in an offensive or rude way',notes:'粗野下流的',examples:'dont you do nothing lewd to gohan'},
+            {word:'exert',edefinition:'',notes:'运用',examples:'he exerts all his strength to lift a rock'},
+            {word:'overexert',edefinition:'',notes:'过度用力',examples:''}
+            ],phrases:[
+            {phrase:'it is not a matter of ',edefinition:'',notes:'不是..的事',examples:'it is not a matter of hair'}
+            ],questions:[
+            '1为什么stand a chance是有机会，stand不是站立的意思么?答：就好比拳击,你没有机会站起来，毫无胜算',
+            'make up有很多种用法，怎么去理解？1make up a story(invent) 2make it up to sb对某人弥补 ',
+            'ask of到底什么意思？答:期待别人去做',
+            '怎么用spar 答:fighting',
+            'delinquent怎么理解?答:年轻罪犯',
+            'lewd我也不理解',
+            'you ve practically got it down,practicing by yourself is plenty get down 什么意思?get it down就是拿下的意思，表示做的很好，学得很好'
+            ]},
+            {
+                no:4,words:[
+                    {word:'mob',edefinition:'noun: a large group or crowd of people who are angry or violent or difficult to control;verb:of a group of people : to move close to (someone) in an excited way',notes:'名词:一大群情绪激动的人;动词:（一大群人）接近',examples:'if that happens,we will be constantly mobbed by the TV reporters'}
+                ],
+                phrases:[
+                    {phrase:'walk out',edefinition:'to leave somewhere suddenly especially as a way of showing disapproval',notes:'表达抗议不赞同而离开',examples:'His racist remark caused many people in the audience to walk out.'}
+                ],
+                questions:[
+                    '美国人口头会说walk out on sb，比如your dad walk out on you,这个为什么是非正式呢'
+                ]
+
+            }
         ]},
         {id:2,mega:'科学电磁炮',episodes:[
             {no:'1',words:[]},
